d8293646264f5725c7cb0868861755ca5eca49bc
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2023-11-27 20:27:03 +0200

    fix typehints

--- font-atlas-stubs/__init__.pyi
+++ font-atlas-stubs/__init__.pyi
@@ -2,8 +2,8 @@ from typing import List, Tuple
 
 def load_font(
     size: Tuple[int, int],
-    fonts: List[str],
-    font_sizes: List[int],
+    fonts: List[bytes],
+    font_sizes: List[float],
     code_points: List[int],
     oversampling: int = 1,
     padding: int = 1,
